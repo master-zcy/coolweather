35f8eb62dbe1c1c4b272cf01d3d0f64999f9197c
zcy <840355496@qq.com>
2018-04-19 15:41:44 +0800

    新增切换城市和手动更新天气的功能

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -57,12 +57,19 @@ public class ChooseAreaActivity extends Activity {
 	// 当前选中的级别
 	private int currentLevel;
 
+	/*
+	 *是否从WeatherActivity中跳转过来
+	 */
+	private boolean isFromWeatherActivity;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		SharedPreferences preferences=PreferenceManager.getDefaultSharedPreferences(this);
-		if(preferences.getBoolean("city_selectd", false)){
+		isFromWeatherActivity=getIntent().getBooleanExtra("form_weather_activity",false);
+		
+		//已经选择了城市且不是从weatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if(preferences.getBoolean("city_selectd", false)&&!isFromWeatherActivity){
 			Intent intent=new Intent(this,WeatherActivity.class);
 			startActivity(intent);
 			finish();
@@ -273,6 +280,10 @@ public class ChooseAreaActivity extends Activity {
 		}else if(currentLevel==LEVEL_CITY){
 			queryProvinces();
 		}else{
+			if(isFromWeatherActivity){
+				Intent intent=new Intent(this,WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
